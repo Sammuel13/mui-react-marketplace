0fc83bb5d6a26abfbcb8230dbe89b6c180aaba79
Sammuel13 <sammluna13@gmail.com>
2023-10-24 23:21:53 -0300

    merge

--- src/App.js
+++ src/App.js
@@ -12,6 +12,25 @@ function App() {
         document.title = "Marketplace";
     }, []);
 
+    const styles = {
+        container: {
+            display: "grid",
+            gridTemplateRows: "auto 1fr auto", // Cria três linhas: cabeçalho, conteúdo e rodapé
+            minHeight: "100vh",
+        },
+        content: {
+            padding: "20px",
+        },
+
+        footer: {
+            display: "flex",
+            justifyContent: "center", // Alinha horizontalmente ao centro
+            alignItems: "center", // Alinha verticalmente ao centro
+            padding: "10px",
+            backgroundColor: "lightgray",
+        },
+    };
+
     return (
         <ThemeProvider theme={theme}>
             <Stack maxWidth="xl">
--- src/components/appbar/appbarDesktop.js
+++ src/components/appbar/appbarDesktop.js
@@ -1,24 +1,279 @@
-import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import { AppbarContainer, AppbarHeader, MyList } from "../../styles/appbar";
-import SearchIcon from "@mui/icons-material/Search";
-import Actions from "./actions";
+// import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+// import { AppbarContainer, AppbarHeader, MyList } from "../../styles/appbar";
+// import SearchIcon from "@mui/icons-material/Search";
+// import Actions from "./actions";
 
-export default function AppbarDesktop({ matches }) {
+// export default function AppbarDesktop({ matches }) {
+//     return (
+//         <AppbarContainer>
+//             <AppbarHeader>Marketplace</AppbarHeader>
+//             <MyList type="row">
+//                 <ListItemText primary="Home" />
+//                 <ListItemText primary="Categories" />
+//                 <ListItemText primary="Products" />
+//                 <ListItemText primary="Contact Us" />
+//                 <ListItemButton>
+//                     <ListItemIcon>
+//                         <SearchIcon />
+//                     </ListItemIcon>
+//                 </ListItemButton>
+//             </MyList>
+//             <Actions matches={matches} />
+//         </AppbarContainer>
+//     );
+// }
+
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import { useUIContext } from "../../context/ui";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Login from "../login";
+// import MoreIcon from "@mui/icons-material/MoreVert";
+
+const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    //marginLeft: 0,
+    minWidth: "60%",
+    [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(3),
+        width: "auto",
+    },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+            width: "100%",
+        },
+    },
+}));
+
+export default function AppbarDesktop() {
+
+    const [isLoginOpen, setIsLoginOpen] = React.useState(false); // Adiciona um estado para controlar a visibilidade do Login
+
+    const handleProfileMenuClick = () => {
+        setIsLoginOpen(true); // Define o estado para true quando o ícone de perfil é clicado
+    };
+  
+    const handleLoginClose = () => {
+        setIsLoginOpen(false); // Define o estado para false quando o Login é fechado
+    };
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    // const handleMobileMenuOpen = (event) => {
+    //     setMobileMoreAnchorEl(event.currentTarget);
+    // };
+
+    const menuId = "primary-search-account-menu";
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        </Menu>
+    );
+
+    const mobileMenuId = "primary-search-account-menu-mobile";
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+            }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+            }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+        >
+            <MenuItem>
+                <IconButton
+                    size="large"
+                    aria-label="show 4 new mails"
+                    color="inherit"
+                >
+                    <Badge badgeContent={4} color="error">
+                        <MailIcon />
+                    </Badge>
+                </IconButton>
+                <p>Messages</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton
+                    size="large"
+                    aria-label="show 17 new notifications"
+                    color="inherit"
+                >
+                    <Badge badgeContent={17} color="error">
+                        <NotificationsIcon />
+                    </Badge>
+                </IconButton>
+                <p>Notifications</p>
+            </MenuItem>
+            <MenuItem onClick={handleProfileMenuOpen}>
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+                <p>Profile</p>
+            </MenuItem>
+        </Menu>
+    );
+
+    const { setIsDrawerOpen } = useUIContext();
+
     return (
-        <AppbarContainer>
-            <AppbarHeader>Marketplace</AppbarHeader>
-            <MyList type="row">
-                <ListItemText primary="Home" />
-                <ListItemText primary="Categories" />
-                <ListItemText primary="Products" />
-                <ListItemText primary="Contact Us" />
-                <ListItemButton>
-                    <ListItemIcon>
-                        <SearchIcon />
-                    </ListItemIcon>
-                </ListItemButton>
-            </MyList>
-            <Actions matches={matches} />
-        </AppbarContainer>
+        <Box sx={{ flexGrow: 1, marginBottom: 5 }}>
+            <AppBar position="static" sx={{ backgroundColor: "#FFCB05" }}>
+                <Toolbar sx={{ justifyContent: "space-between" }}>
+                    <Box sx={{ display: "flex", alignItems: "center" }}>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="open drawer"
+                            sx={{ mr: 2 }}
+                            onClick={() => setIsDrawerOpen(true)}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ display: { xs: "none", sm: "block" } }}
+                        >
+                            PokeMarketplace
+                        </Typography>
+                    </Box>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ "aria-label": "search" }}
+                            sx={{ width: "100%" }}
+                        />
+                    </Search>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "16px",
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-label="show 4 new mails"
+                            color="inherit"
+                        >
+                            <Badge badgeContent={4} color="error">
+                                <MailIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton
+                            size="large"
+                            aria-label="show 17 new notifications"
+                            color="inherit"
+                        >
+                            <Badge badgeContent={17} color="error">
+                                <NotificationsIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton
+                            size="large"
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileMenuClick}
+                            color="inherit"
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                        <Login isOpens={isLoginOpen} onClose={handleLoginClose} />
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            {renderMobileMenu}
+            {renderMenu}
+        </Box>
     );
 }
